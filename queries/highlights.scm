d40b80776bdc54ff0ab14fabc2db4f2eba7d6740
LaBatata101 <labatata101@gmail.com>
2024-06-01 19:14:08 -0300

    Add more highlights for nodes

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,17 +1,117 @@
+(identifier) @variable
+
+(fun_function_definition
+  name: (identifier) @function)
+(imp_function_definition
+  name: (identifier) @function)
+(parameters) @variable.parameter
+
+(object_definition
+  name: (identifier) @type)
+(object_field
+  (identifier) @variable.member)
+
+(imp_type_definition
+  name: (identifier) @type)
+(imp_type_constructor
+  (identifier) @constructor)
+(imp_type_constructor_field
+  (identifier) @variable.member)
+
+(fun_type_definition
+  name: (identifier) @type)
+(fun_type_constructor
+  (identifier) @constructor)
+(fun_type_constructor_fields
+  (identifier) @variable.member)
+
+(constructor
+  (identifier) @constructor)
+(constructor
+  field: (identifier) @property)
+
+(call_expression
+  (identifier) @function.call)
+
+(switch_case
+  (switch_pattern) @character.special
+  (#eq? @character.special "_"))
+
+(integer) @number
+(float) @number.float
+
+(character) @character
+(comment) @comment
 [
-  "@"
-  "λ"
-  "def"
+ (symbol)
+ (string)
+] @string
+
+[
+  ":"
+  ","
+  ";"
+] @punctuation.delimiter
+
+[
+  "["
+  "]"
+  "("
+  ")"
+  "{"
+  "}"
+] @punctuation.bracket
+
+[
+  "-"
+  "-="
+  "!="
+  "*"
+  "**"
+  "*="
+  "/"
+  "/="
+  "&"
+  "%"
+  "^"
+  "+"
+  "+="
+  "<"
+  "="
+  "=="
+  ">"
+  "|"
+  "~"
+  "&"
+  "<-"
+] @operator
+
+[
+  "if"
   "elif"
   "else"
-  "if"
+] @keyword.conditional
+
+
+[
+ "def"
+  "@"
+  "λ"
   "lambda"
-  "return"
+] @keyword.function
+
+"return" @keyword.return
+"for" @keyword.repeat
+
+[
+  "object"
+  "type"
+] @keyword.type
+
+[
   "with"
   "match"
   "case"
-  "object"
-  "type"
   "open"
   "use"
   "do"
@@ -21,6 +121,5 @@
   "switch"
   "ask"
   "let"
-  "for"
   "in"
 ] @keyword
